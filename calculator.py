c8bd0bf1637171d627e00d25bb13f6988ed7d220
Uwabeza Ejidia <uwabezaejidia94@gmail.com>
2026-01-31 10:41:02 +0300

    added a fuction for dividing on the calculator

--- calculator.py
+++ calculator.py
@@ -9,3 +9,10 @@ def subtract(x, y):
 def multiply(x, y):
     """Multiply two numbers"""
     return x * y
+
+def divide(x, y):
+    """Divide two numbers (handle division by zero)"""
+    if y == 0:
+        raise ValueError("Error: Division by zero is not allowed")
+    return x / y
+
